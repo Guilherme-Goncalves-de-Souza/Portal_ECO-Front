188cf1c5198bd751835f7b1080d698e9f637c9d5
GGS <gui020fla@outlook.com>
2024-12-30 17:16:25 -0300

    Adicionando projeto

--- src/components/ImageCarousel/ImageCarousel.tsx
+++ src/components/ImageCarousel/ImageCarousel.tsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect, useRef } from 'react';
 import './ImageCarousel.css';
 
 // Importe as imagens no topo do arquivo
-import PORTAL_ECO from '../../assets/carousel/PORTAL_ECO.png';
-import image1 from '../../assets/carousel/image1.jpg';
-import image2 from '../../assets/carousel/image2.jpg';
-import image3 from '../../assets/carousel/image3.jpg';
-import image4 from '../../assets/carousel/image4.jpg';
+import PORTAL_ECO from '../../assets/PORTAL_ECO.png';
+import image1 from '../../assets/image1.jpg';
+import image2 from '../../assets/image2.jpg';
+import image3 from '../../assets/image3.jpg';
+import image4 from '../../assets/image4.jpg';
 
 const ImageCarousel: React.FC = () => {
   const images: string[] = [PORTAL_ECO, image1, image2, image3, image4];
