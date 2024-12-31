2a971b4d8516c19b14c7186c09bf923bf46548c7
GGS <gui020fla@outlook.com>
2024-12-31 07:51:21 -0300

    Imagem

--- src/pages/Dicas/Dicas.tsx
+++ src/pages/Dicas/Dicas.tsx
@@ -1,3 +1,12 @@
+// Importe as imagens no topo do arquivo
+import recicleLixo from "../../../images/recicle_lixo.jpg";
+import energia from "../../../images/energia.jpg";
+import menosPlastico from "../../../images/menos_plastico.jpg";
+import co2 from "../../../images/co2.jpg";
+import reciclando from "../../../images/reciclando.jpeg";
+import arvore from "../../../images/arvore.png";
+import agua from "../../../images/agua.jpg";
+
 import "./Dicas.css";
 
 const Dicas = () => {
@@ -6,48 +15,48 @@ const Dicas = () => {
       id: 1,
       titulo: "Vamos Reciclar!",
       descricao: "Participe de programas de reciclagem do Portal ECO.",
-      imagem: "../../../images/recicle_lixo.jpg",
+      imagem: recicleLixo,
     },
     {
       id: 2,
       titulo: "Cuide da Energia!",
       descricao:
         "Desligue os aparelhos eletrônicos da tomada quando não estiverem em uso.",
-      imagem: "../../../images/energia.jpg",
+      imagem: energia,
     },
     {
       id: 3,
       titulo: "Menos Plástico!",
       descricao:
         "Prefira produtos a granel para evitar embalagens desnecessárias.",
-      imagem: "../../../images/menos_plastico.jpg",
+      imagem: menosPlastico,
     },
     {
       id: 4,
       titulo: "⚠️CO2⚠️",
       descricao:
         "Utilize transporte público, bicicleta ou carona sempre que possível.",
-      imagem: "../../../images/co2.jpg",
+      imagem: co2,
     },
     {
       id: 5,
       titulo: "Faça como nós!",
       descricao: "Participe de programas de reciclagem do Portal ECO.",
-      imagem: "../../../images/reciclando.jpeg",
+      imagem: reciclando,
     },
     {
       id: 6,
       titulo: "Ajude a criar novas árvores.",
       descricao:
         "Plante uma árvore e ajude a aumentar as áreas verdes do planeta.",
-      imagem: "../../../images/arvore.png",
+      imagem: arvore,
     },
     {
       id: 7,
       titulo: "Economize ÁGUA!",
       descricao:
         "Economize água: reutilize a água da chuva para regar plantas.",
-      imagem: "../../../images/agua.jpg",
+      imagem: agua,
     },
   ];
 
